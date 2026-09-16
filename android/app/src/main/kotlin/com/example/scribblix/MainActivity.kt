74e75a246f7a94a5b1e38815f698db24eafc605b
sivatharan-Devs <sivatharan2022@gmail.com>
2026-09-16 18:13:34 +0530

    chore: Initialize Scribblix project

---
+++ android/app/src/main/kotlin/com/example/scribblix/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.scribblix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'Scribblix',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Text('Starting of Scribblix app design'),
+      ),
+    ),
+  );
+}
